e72389937826f9de40c12d8d5097d92dcc589fa0
Rik de Wijn <rdwijn@nl.pamgene.com>
2018-07-03 09:41:36 +0200

    ++

--- testcombat.r
+++ testcombat.r
@@ -2,7 +2,6 @@ library(pgBatch)
 library(R6)
 X = matrix(nrow = 100, ncol = 20, data = rnorm(20*100))
 bx = as.factor(c( rep(1,10) ,rep(2,10) ))
-grp =
 cMod = pgCombat$new()
 cMod = cMod$fit(X, bx,ref.batch = "1")
 Xc = cMod$apply(X, bx)
